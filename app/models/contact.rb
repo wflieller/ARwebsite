4be10738a201833aa360fceb1d4da5daa711069b
Walker Flieller <wflieller@gmail.com>
2015-11-04 09:19:35 -0600

    Initial Commit

---
+++ app/models/contact.rb
@@ -0,0 +1,18 @@
+class Contact < MailForm::Base
+  attribute :name,      :validate => true
+  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :number,    :validate => true
+  attribute :occupation,:validate => true
+  attribute :message
+  attribute :nickname,  :captcha  => true
+
+  # Declare the e-mail headers. It accepts anything the mail method
+  # in ActionMailer accepts.
+  def headers
+    {
+      :subject => "New Contact Form",
+      :to => "wflieller@gmail.com",
+      :from => %("#{name}" <#{email}>)
+    }
+  end
+end
